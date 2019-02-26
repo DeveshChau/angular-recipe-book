366602900e05c95ee7b17519127cb5c2ea5958a1
DeveshChau <deveshachaudhari@gmail.com>
2019-02-26 16:02:12 +0530

    component communcation for recipe description and shopping list addtion

--- src/app/components/recipes/recipe-list/recipe-list.component.ts
+++ src/app/components/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
 import { Recipe } from '../../../models/recipe.model';
 
 @Component({
@@ -8,6 +8,7 @@ import { Recipe } from '../../../models/recipe.model';
 })
 export class RecipeListComponent implements OnInit {
 
+  @Output() recipeWasSelected = new EventEmitter<Recipe>()
   recipes: Recipe[] = [
     new Recipe("Veg Burger","Home made veg burger simply delicious to taste. The burger buns are stuffed with vegetable patties, spiced mayonnaise dressing and cucumber, tomato, onion slices","https://susanstable.com/wp-content/uploads/veggie-burger-easy-4-copy.jpg"),
     new Recipe("Veg Pizza","This is a tried and tested recipe. You will be able to make great pizzas with a choice of your toppings","https://www.vegrecipesofindia.com/wp-content/uploads/2012/04/veg-pizza-recipe-1.jpg")
@@ -15,6 +16,9 @@ export class RecipeListComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
+  }
+  onRecipeSelect(recipeEl: Recipe){
+    this.recipeWasSelected.emit(recipeEl)
   }
 
 }
--- src/app/components/shopping-list/shopping-list.component.ts
+++ src/app/components/shopping-list/shopping-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 import { Ingredient } from '../../models/ingredient.model';
 
 @Component({
@@ -15,6 +15,10 @@ export class ShoppingListComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
+  }
+
+  onIngredientAdded(ingredient: Ingredient){
+    this.ingredients.push(ingredient)
   }
 
 }
