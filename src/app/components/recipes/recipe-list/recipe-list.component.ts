2eb39b5fe1e3fd558dc7a0dd4a859aecb43e6ef5
DeveshChau <deveshachaudhari@gmail.com>
2019-02-26 12:47:59 +0530

    add recipe list, recipe details , shopping list and shopping list edit

--- src/app/components/recipes/recipe-list/recipe-list.component.ts
+++ src/app/components/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../../../models/recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
@@ -7,6 +8,10 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RecipeListComponent implements OnInit {
 
+  recipes: Recipe[] = [
+    new Recipe("Veg Burger","Home made veg burger simply delicious to taste. The burger buns are stuffed with vegetable patties, spiced mayonnaise dressing and cucumber, tomato, onion slices","https://susanstable.com/wp-content/uploads/veggie-burger-easy-4-copy.jpg"),
+    new Recipe("Veg Pizza","This is a tried and tested recipe. You will be able to make great pizzas with a choice of your toppings","https://www.vegrecipesofindia.com/wp-content/uploads/2012/04/veg-pizza-recipe-1.jpg")
+  ]
   constructor() { }
 
   ngOnInit() {
