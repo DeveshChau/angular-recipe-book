f6f7e74d76c36713d6792cd73a3bb2575a4d7dc0
DeveshChau <deveshachaudhari@gmail.com>
2019-04-30 10:43:18 +0530

    updates to deploy to heroku

--- src/app/components/recipes/recipe.service.ts
+++ src/app/components/recipes/recipe.service.ts
@@ -1,25 +1,26 @@
-import { Injectable } from "@angular/core";
-import { Recipe } from "src/app/models/recipe.model";
-import { ShoppingListService } from "../shopping-list/shoppingList.service";
-import { Ingredient } from "src/app/models/ingredient.model";
-import { Subject } from "rxjs";
+import { Injectable } from '@angular/core';
+import { Recipe } from 'src/app/models/recipe.model';
+import { ShoppingListService } from '../shopping-list/shoppingList.service';
+import { Ingredient } from 'src/app/models/ingredient.model';
+import { Subject } from 'rxjs';
 
-@Injectable() 
+@Injectable()
 export class RecipeService {
 
     recipeChanged = new Subject<Recipe[]>()
-    
+
     private recipe: Recipe[] = [
-        new Recipe("Veg Burger",
-        "Home made veg burger simply delicious to taste. The burger buns are stuffed with vegetable patties, spiced mayonnaise dressing and cucumber, tomato, onion slices",
-        "https://susanstable.com/wp-content/uploads/veggie-burger-easy-4-copy.jpg",
+        new Recipe('Veg Burger',
+        'Home made veg burger simply delicious to taste.'
+        + ' The burger buns are stuffed with vegetable patties, spiced mayonnaise dressing and cucumber, tomato, onion slices',
+        'https://susanstable.com/wp-content/uploads/veggie-burger-easy-4-copy.jpg',
         [
             new Ingredient('onion',10),
             new Ingredient('tomato', 6)
         ]),
-        new Recipe("Veg Pizza",
-        "This is a tried and tested recipe. You will be able to make great pizzas with a choice of your toppings",
-        "https://www.vegrecipesofindia.com/wp-content/uploads/2012/04/veg-pizza-recipe-1.jpg",
+        new Recipe('Veg Pizza',
+        'This is a tried and tested recipe. You will be able to make great pizzas with a choice of your toppings',
+        'https://www.vegrecipesofindia.com/wp-content/uploads/2012/04/veg-pizza-recipe-1.jpg',
         [
             new Ingredient('onion', 10),
             new Ingredient('tomato', 6)
@@ -27,12 +28,12 @@ export class RecipeService {
     ]
 
     constructor(private slService: ShoppingListService) {}
-    
-    setRecipe(recipes: Recipe[]){
-        this.recipe = recipes
-        this.recipeChanged.next(this.recipe.slice())
+
+    setRecipe(recipes: Recipe[]) {
+        this.recipe = recipes;
+        this.recipeChanged.next(this.recipe.slice());
     }
-    
+
     getRecipe(){
         return this.recipe.slice();
     }
--- src/app/shared/data-storage.service.ts
+++ src/app/shared/data-storage.service.ts
@@ -1,37 +1,37 @@
-import { Injectable } from "@angular/core";
-import { Http, Response } from "@angular/http";
-import { RecipeService } from "../components/recipes/recipe.service";
-import { Recipe } from "../models/recipe.model";
-import { map } from 'rxjs/operators'
-import { AuthService } from "../components/auth/auth.service";
+import { Injectable } from '@angular/core';
+import { Http, Response } from '@angular/http';
+import { RecipeService } from '../components/recipes/recipe.service';
+import { Recipe } from '../models/recipe.model';
+import { map } from 'rxjs/operators';
+import { AuthService } from '../components/auth/auth.service';
 
 @Injectable()
 export class DataStorageService {
-    constructor(private http: Http, 
+    constructor(private http: Http,
         private recipeService: RecipeService,
-        private authService: AuthService){}
+        private authService: AuthService) {}
 
     storeRecipes() {
-        const token = this.authService.getToken()
-        return this.http.put('https://angular-recipe-book-1b0ca.firebaseio.com/recipes.json?auth='+token, this.recipeService.getRecipe())
+        const token = this.authService.getToken();
+        return this.http.put('https://angular-recipe-book-1b0ca.firebaseio.com/recipes.json?auth=' + token, this.recipeService.getRecipe());
     }
 
     getRecipes() {
-        const token = this.authService.getToken()
+        const token = this.authService.getToken();
 
-        this.http.get("https://angular-recipe-book-1b0ca.firebaseio.com/recipes.json?auth="+token)
+        this.http.get('https://angular-recipe-book-1b0ca.firebaseio.com/recipes.json?auth=' + token)
         .pipe(map(
-            (response: Response) => {
-                const recipes: Recipe[] = response.json()
-                for(let recipe of recipes) {
-                    if(!recipe['ingredients']){
-                        recipe['ingredients'] = []
+            (response:Response) => {
+                const recipes: Recipe[] = response.json();
+                for (const recipe of recipes) {
+                    if (!recipe['ingredients']) {
+                        recipe['ingredients'] = [];
                     }
-                } 
-                return recipes
+                }
+                return recipes;
             }
         )).subscribe((recipes: Recipe[]) => {
-            this.recipeService.setRecipe(recipes)
-        })
+            this.recipeService.setRecipe(recipes);
+        });
     }
 }
